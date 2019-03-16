712148bc99c269ddedf80255100661721f52e2f4
Jesús Armenta <Jesus.armenta1993@outlook.com>
2019-03-15 21:58:21 -0700

    55

--- Hackaton2019/Web/src/CleanArchitecture.Core/Entities/LaborParcela.cs
+++ Hackaton2019/Web/src/CleanArchitecture.Core/Entities/LaborParcela.cs
@@ -10,10 +10,20 @@ namespace CleanArchitecture.Core.Entities
         public DateTime? InicioReal { get; set; }
         public DateTime? FinReal { get; set; }
 
+        public double Costo { get; set; }
+        public EstatusLabor Estatus { get; set; }
+        public string Comentario { get; set; }
+
         public int LaborId { get; set; }
         public Labor Labor { get; set; }
 
         public int PlanSiembraParcelaId { get; set; }
         public PlanSiembraParcela PlanSiembraParcela { get; set; }
+    }
+
+    public enum EstatusLabor
+    {
+        Activa,
+        Cerrada,
     }
 }
---
+++ Hackaton2019/Web/src/CleanArchitecture.Infrastructure/Migrations/20190316045611_2.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace CleanArchitecture.Infrastructure.Migrations
+{
+    public partial class _2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Comentario",
+                table: "LaboresParcela",
+                nullable: true);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Costo",
+                table: "LaboresParcela",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<int>(
+                name: "Estatus",
+                table: "LaboresParcela",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Comentario",
+                table: "LaboresParcela");
+
+            migrationBuilder.DropColumn(
+                name: "Costo",
+                table: "LaboresParcela");
+
+            migrationBuilder.DropColumn(
+                name: "Estatus",
+                table: "LaboresParcela");
+        }
+    }
+}
