cf53b9cf127159415b53fbce8713a404c1f65fa8
Jesús Armenta <Jesus.armenta1993@outlook.com>
2019-03-16 04:35:30 -0700

    jd

--- Hackaton2019/Web/src/CleanArchitecture.Web/Controllers/HomeController.cs
+++ Hackaton2019/Web/src/CleanArchitecture.Web/Controllers/HomeController.cs
@@ -19,6 +19,11 @@ namespace CleanArchitecture.Web.Controllers
             return View();
         }
 
+        public IActionResult CapturarCostos()
+        {
+            return View();
+        }
+
         public IActionResult Error()
         {
             return View();
